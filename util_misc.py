c32639c70e5b4224d078fdc948b3009eb2e99c95
Pablo Acosta-Serafini <pmasdev@gmail.com>
2013-11-22 17:13:54 -0500

    Initial commit of util_misc module

---
+++ util_misc.py
@@ -0,0 +1,21 @@
+﻿"""
+Miscellaneous utility classes, methods, functions and constants
+"""
+
+def pcolor(text, color, tab=0):
+	"""
+	Print to terminal in a limited amount of colors
+	"""
+	esc_dict = {'black':30, 'red':31, 'green':32, 'yellow':33, 'blue':34, 'magenta':35, 'cyan':36, 'white':37}
+	if isinstance(text, str) is False:
+		raise TypeError('text has to be a string in pcolor function')
+	if isinstance(color, str) is False:
+		raise TypeError('color has to be a string in pcolor function')
+	if isinstance(tab, int) is False:
+		raise TypeError('tab has to be an intenger in pcolor function')
+	color = color.lower()
+	if color not in esc_dict.keys():
+		raise RuntimeError('Color '+color+' not supported by pcolor function')
+	return '\033['+str(esc_dict[color])+'m'+' '*tab+text+'\033[0m'
+
+
