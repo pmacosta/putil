5def9c287454e9e99991a48f43532691d19f4367
Pablo Acosta-Serafini <pmasdev@gmail.com>
2013-11-25 19:56:22 -0500

    Added new methods: * isnumber, check if argument is a number * isreal, check if argument is float or integer * per, percentage difference between two numbers or element-wise Numpy vectors

--- util_misc.py
+++ util_misc.py
@@ -2,6 +2,8 @@
 Miscellaneous utility classes, methods, functions and constants
 """
 
+import numpy
+
 def pcolor(text, color, tab=0):
 	"""
 	Print to terminal in a limited amount of colors
@@ -18,4 +20,33 @@ def pcolor(text, color, tab=0):
 		raise RuntimeError('Color '+color+' not supported by pcolor function')
 	return '\033['+str(esc_dict[color])+'m'+' '*tab+text+'\033[0m'
 
+def isnumber(num):
+	"""
+	Checks if the argument is a number: complex, float or integer
+	"""
+	return True if (isinstance(num, int) is True) or (isinstance(num, float) is True) or (isinstance(num, complex) is True) else False
 
+def isreal(num):
+	"""
+	Checks if the argument is a real number: float or integer
+	"""
+	return True if (isinstance(num, int) is True) or (isinstance(num, float) is True) else False
+
+def per(numa, numb):
+	"""
+	Calculates percentage difference between two numbers
+	"""
+	numa_type = 1 if isreal(numa) is True else (2 if isinstance(numa, numpy.ndarray) is True else 0)
+	numb_type = 1 if isreal(numb) is True else (2 if isinstance(numb, numpy.ndarray) is True else 0)
+	if numa_type != numb_type:
+		raise TypeError('Arguments are not of the same type in function per')
+	if numa_type == 1:
+		num_max = max(numa, numb)
+		num_min = min(numa, numb)
+		num_min = num_min if num_min != 0 else 1e-20
+		return (num_max/num_min)-1
+	else:
+		num_max = numpy.maximum(numa, numb)
+		num_min = numpy.minimum(numa, numb)
+		num_min[num_min == 0] = 1e-20
+		return (num_max/num_min)-1
