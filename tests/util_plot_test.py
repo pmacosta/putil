9acaa8fcd6578fc651f42dfcffa6a08ebebff169
Pablo Acosta-Serafini <pmasdev@gmail.com>
2014-04-11 07:20:48 -0400

    Refactored existing unit tests for util_plot module, added more documentation and coverage

--- tests/util_plot_test.py
+++ tests/util_plot_test.py
@@ -12,102 +12,153 @@ def test_basic_source_indep_min_type():	#pylint: disable-msg=C0103
 	"""
 	Tests indep_min type validation
 	"""
+	comp = list()
 	# __init__ path
-	with pytest.raises(TypeError) as excinfo1:
+	# Wrong types
+	with pytest.raises(TypeError) as excinfo:
 		util_plot.BasicSource(indep_min='a')
-	comp1 = excinfo1.value.message == 'Parameter indep_min is of the wrong type'
-	with pytest.raises(TypeError) as excinfo2:
+	comp.append(excinfo.value.message == 'Parameter indep_min is of the wrong type')
+	with pytest.raises(TypeError) as excinfo:
 		util_plot.BasicSource(indep_min=False)
-	comp2 = excinfo2.value.message == 'Parameter indep_min is of the wrong type'
-	util_plot.BasicSource(indep_min=None)
-	util_plot.BasicSource(indep_min=1)
-	util_plot.BasicSource(indep_min=2.0)
+	comp.append(excinfo.value.message == 'Parameter indep_min is of the wrong type')
+	# Valid values, these should not raise an exception
+	comp.append(util_plot.BasicSource(indep_min=None).indep_min == None)
+	comp.append(util_plot.BasicSource(indep_min=1).indep_min == 1)
+	comp.append(util_plot.BasicSource(indep_min=2.0).indep_min == 2.0)
 	# Managed attribute path
+	# Wrong types
 	obj = util_plot.BasicSource()
-	with pytest.raises(TypeError) as excinfo3:
+	with pytest.raises(TypeError) as excinfo:
 		obj.indep_min = 'a'
-	comp3 = excinfo3.value.message == 'Parameter indep_min is of the wrong type'
-	with pytest.raises(TypeError) as excinfo4:
+	comp.append(excinfo.value.message == 'Parameter indep_min is of the wrong type')
+	with pytest.raises(TypeError) as excinfo:
 		obj.indep_min = False
-	comp4 = excinfo4.value.message == 'Parameter indep_min is of the wrong type'
+	comp.append(excinfo.value.message == 'Parameter indep_min is of the wrong type')
+	# Valid values, these should not raise an exception
 	obj.indep_min = None
+	comp.append(obj.indep_min == None)
 	obj.indep_min = 1
+	comp.append(obj.indep_min == 1)
 	obj.indep_min = 2.0
-	assert (comp1, comp2, comp3, comp4) == (True, True, True, True)
+	comp.append(obj.indep_min == 2.0)
+	assert comp == 10*[True]
 
 def test_basic_source_indep_max_type():	#pylint: disable-msg=C0103
 	"""
 	Tests indep_max type validation
 	"""
+	comp = list()
 	# __init__ path
-	with pytest.raises(TypeError) as excinfo1:
+	# Wrong types
+	with pytest.raises(TypeError) as excinfo:
 		util_plot.BasicSource(indep_max='a')
-	comp1 = excinfo1.value.message == 'Parameter indep_max is of the wrong type'
-	with pytest.raises(TypeError) as excinfo2:
+	comp.append(excinfo.value.message == 'Parameter indep_max is of the wrong type')
+	with pytest.raises(TypeError) as excinfo:
 		util_plot.BasicSource(indep_max=False)
-	comp2 = excinfo2.value.message == 'Parameter indep_max is of the wrong type'
-	util_plot.BasicSource(indep_max=None)
-	util_plot.BasicSource(indep_max=1)
-	util_plot.BasicSource(indep_max=2.0)
+	comp.append(excinfo.value.message == 'Parameter indep_max is of the wrong type')
+	# Valid values, these should not raise an exception
+	comp.append(util_plot.BasicSource(indep_max=None).indep_max == None)
+	comp.append(util_plot.BasicSource(indep_max=1).indep_max == 1)
+	comp.append(util_plot.BasicSource(indep_max=2.0).indep_max == 2.0)
 	# Managed attribute path
+	# Wrong types
 	obj = util_plot.BasicSource()
-	with pytest.raises(TypeError) as excinfo3:
+	with pytest.raises(TypeError) as excinfo:
 		obj.indep_max = 'a'
-	comp3 = excinfo3.value.message == 'Parameter indep_max is of the wrong type'
-	with pytest.raises(TypeError) as excinfo4:
+	comp.append(excinfo.value.message == 'Parameter indep_max is of the wrong type')
+	with pytest.raises(TypeError) as excinfo:
 		obj.indep_max = False
-	comp4 = excinfo4.value.message == 'Parameter indep_max is of the wrong type'
+	comp.append(excinfo.value.message == 'Parameter indep_max is of the wrong type')
+	# Valid values, these should not raise an exception
 	obj.indep_max = None
+	comp.append(obj.indep_max == None)
 	obj.indep_max = 1
+	comp.append(obj.indep_max == 1)
 	obj.indep_max = 2.0
-	assert (comp1, comp2, comp3, comp4) == (True, True, True, True)
+	comp.append(obj.indep_max == 2.0)
+	assert comp == 10*[True]
 
 def test_basic_source_indep_min_greater_than_indep_max():	#pylint: disable-msg=C0103
 	"""
 	Test if object behaves correctly when indep_min and indep_max are incongrous
 	"""
+	comp = list()
+	# Assign indep_min first
 	obj = util_plot.BasicSource(indep_min=45)
-	with pytest.raises(ValueError) as excinfo1:
+	with pytest.raises(ValueError) as excinfo:
 		obj.indep_max = 0
-	comp1 = excinfo1.value.message == 'Parameter indep_min is greater than parameter indep_max'
-	obj.indep_min = None
+	comp.append(excinfo.value.message == 'Parameter indep_min is greater than parameter indep_max')
+	# Assign indep_max first
+	obj = util_plot.BasicSource()
 	obj.indep_max = 40
-	with pytest.raises(ValueError) as excinfo2:
+	with pytest.raises(ValueError) as excinfo:
 		obj.indep_min = 50
-	comp2 = excinfo2.value.message == 'Parameter indep_min is greater than parameter indep_max'
-	assert (comp1, comp2) == (True, True)
+	comp.append(excinfo.value.message == 'Parameter indep_min is greater than parameter indep_max')
+	assert comp == 2*[True]
 
 def test_basic_source_indep_var_type():	#pylint: disable-msg=C0103
 	"""
 	Tests indep_var type validation
 	"""
+	comp = list()
 	# __init__ path
-	with pytest.raises(TypeError) as excinfo1:
+	# Wrong type
+	with pytest.raises(TypeError) as excinfo:
 		util_plot.BasicSource(indep_var='a')
-	comp1 = excinfo1.value.message == 'Parameter indep_var is of the wrong type'
-	with pytest.raises(ValueError) as excinfo2:
+	comp.append(excinfo.value.message == 'Parameter indep_var is of the wrong type')
+	# Non monotonically increasing vector
+	with pytest.raises(ValueError) as excinfo:
 		util_plot.BasicSource(indep_var=numpy.array([1.0, 2.0, 0.0, 3.0]))
-	comp2 = excinfo2.value.message == 'Parameter indep_var is not an increasing Numpy vector'
-	with pytest.raises(ValueError) as excinfo3:
+	comp.append(excinfo.value.message == 'Parameter indep_var is not an increasing Numpy vector')
+	# Empty vector
+	with pytest.raises(ValueError) as excinfo:
 		util_plot.BasicSource(indep_var=numpy.array([]))
-	comp3 = excinfo3.value.message == 'Parameter indep_var is empty'
+	comp.append(excinfo.value.message == 'Parameter indep_var is empty')
+	# Valid values, these should not raise any exception
+	comp.append(util_plot.BasicSource(indep_var=None).indep_var == None)
+	comp.append((util_plot.BasicSource(indep_var=numpy.array([1, 2, 3])).indep_var == numpy.array([1, 2, 3])).all())
+	comp.append((util_plot.BasicSource(indep_var=numpy.array([4.0, 5.0, 6.0])).indep_var == numpy.array([4.0, 5.0, 6.0])).all)
+	# Invalid thresholding
+	# Assign indep_min via __init__ path
 	obj = util_plot.BasicSource(indep_min=45)
-	with pytest.raises(ValueError) as excinfo4:
+	with pytest.raises(ValueError) as excinfo:
 		obj.indep_var = numpy.array([1, 2, 3])
-	comp4 = excinfo4.value.message == 'Parameter indep_var is empty after indep_min/indep_max thresholding'
+	comp.append(excinfo.value.message == 'Parameter indep_var is empty after indep_min/indep_max thresholding')
+	# Assign indep_min via attribute
 	obj = util_plot.BasicSource(indep_var=numpy.array([1, 2, 3]))
-	with pytest.raises(ValueError) as excinfo5:
+	with pytest.raises(ValueError) as excinfo:
 		obj.indep_min = 10
-	comp5 = excinfo5.value.message == 'Parameter indep_var is empty after indep_min/indep_max thresholding'
-	obj.indep_min = None
-	with pytest.raises(ValueError) as excinfo6:
+	comp.append(excinfo.value.message == 'Parameter indep_var is empty after indep_min/indep_max thresholding')
+	# Assign indep_max via attribute
+	obj = util_plot.BasicSource(indep_max=0)
+	with pytest.raises(ValueError) as excinfo:
+		obj.indep_var = numpy.array([1, 2, 3])
+	comp.append(excinfo.value.message == 'Parameter indep_var is empty after indep_min/indep_max thresholding')
+	# Assign indep_max via attribute
+	obj = util_plot.BasicSource(indep_var=numpy.array([1, 2, 3]))
+	with pytest.raises(ValueError) as excinfo:
 		obj.indep_max = 0
-	comp6 = excinfo6.value.message == 'Parameter indep_var is empty after indep_min/indep_max thresholding'
-	with pytest.raises(ValueError) as excinfo7:
+	comp.append(excinfo.value.message == 'Parameter indep_var is empty after indep_min/indep_max thresholding')
+	# Assign both indep_min and indep_max via __init__ path
+	with pytest.raises(ValueError) as excinfo:
 		util_plot.BasicSource(indep_var=numpy.array([1, 2, 3]), indep_min=4, indep_max=10)
-	comp7 = excinfo7.value.message == 'Parameter indep_var is empty after indep_min/indep_max thresholding'
-	util_plot.BasicSource(indep_var=None)
-	util_plot.BasicSource(indep_var=numpy.array([1, 2, 3]))
-	util_plot.BasicSource(indep_var=numpy.array([1.0, 2.0, 3.0]))
-	assert (comp1, comp2, comp3, comp4, comp5, comp6, comp7) == (True, True, True, True, True, True, True)
+	comp.append(excinfo.value.message == 'Parameter indep_var is empty after indep_min/indep_max thresholding')
+	# Managed attribute path
+	obj = util_plot.BasicSource()
+	# Wrong type
+	with pytest.raises(TypeError) as excinfo:
+		util_plot.BasicSource(indep_var='a')
+	comp.append(excinfo.value.message == 'Parameter indep_var is of the wrong type')
+	# Non monotonically increasing vector
+	with pytest.raises(ValueError) as excinfo:
+		obj.indep_var = numpy.array([1.0, 2.0, 0.0, 3.0])
+	comp.append(excinfo.value.message == 'Parameter indep_var is not an increasing Numpy vector')
+	# Empty vector
+	with pytest.raises(ValueError) as excinfo:
+		obj.indep_var = numpy.array([])
+	comp.append(excinfo.value.message == 'Parameter indep_var is empty')
+	obj.indep_var = None
+	obj.indep_var = numpy.array([1, 2, 3])
+	obj.indep_var = numpy.array([4.0, 5.0, 6.0])
+	assert comp == 14*[True]
 
