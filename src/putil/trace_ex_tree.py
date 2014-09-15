16fd1a6373db6509176d021c93d2326a1822d289
Pablo Acosta-Serafini <pmasdev@gmail.com>
2014-09-14 22:21:19 -0400

    trace_ex_tree.py script:  * Inclusion of all members in Tree class to tracing tree module:  * Inclusion of all members in Tree class to cog tracing  * Alphabetical sorting of some methods()

--- src/putil/trace_ex_tree.py
+++ src/putil/trace_ex_tree.py
@@ -11,41 +11,36 @@ _EXH = None
 def trace_tree(no_print=False):
 	""" Trace Tree class """
 	global _EXH	#pylint: disable=W0603
+	if not no_print:
+		print putil.misc.pcolor('Tracing Tree', 'blue')
 	_EXH = putil.exh.ExHandle(putil.tree.Tree)
 	obj = putil.tree.Tree()
 	obj.add([
-		{'name':'root.branch1', 'data':list()},
-		{'name':'root.branch2', 'data':list()},
-		{'name':'root.branch1.leaf1', 'data':list()},
-		{'name':'root.branch1.leaf1.subleaf1', 'data':333},
-		{'name':'root.branch1.leaf2', 'data':'Hello world!'},
-		{'name':'root.branch1.leaf2.subleaf2', 'data':list()},
+		{'name':'dummy.root.branch1', 'data':list()},
+		{'name':'dummy.root.branch2', 'data':list()},
+		{'name':'dummy.root.branch1.leaf1', 'data':list()},
+		{'name':'dummy.root.branch1.leaf1.subleaf1', 'data':333},
+		{'name':'dummy.root.branch1.leaf2', 'data':'Hello world!'},
+		{'name':'dummy.root.branch1.leaf2.subleaf2', 'data':list()},
 	])
-	print 'Original tree'
-	print str(obj)
-	obj.collapse('root.branch1')
-	print 'Collapsed tree'
-	print str(obj)
-	obj.copy_subtree('root.branch1', 'root.branch3')
-	print 'Copied tree'
-	print str(obj)
-	obj.delete('root.branch2')
-	print 'Deleted tree'
-	print str(obj)
-	obj.flatten_subtree('root.branch1')
-	print 'Flattened tree'
-	print str(obj)
-	obj.get_node('root')
-	obj.get_node_children('root')
-	obj.get_node_parent('root.branch1.leaf1.subleaf1')
-	obj.get_children('root')
-	obj.get_data('root')
-	obj.in_tree('root')
-	obj.is_root('root')
-	obj.is_leaf('root')
-	obj.make_root('root.branch1.leaf2')
-	print 'Make root tree'
-	print str(obj)
+	obj.collapse('dummy.root.branch1')
+	obj.copy_subtree('dummy.root.branch1', 'dummy.root.branch3')
+	obj.delete('dummy.root.branch2')
+	obj.flatten_subtree('dummy.root.branch1')
+	obj.get_children('dummy.root')
+	obj.get_data('dummy.root')
+	obj.get_leafs('dummy.root')
+	obj.get_node('dummy.root')
+	obj.get_node_children('dummy.root')
+	obj.get_node_parent('dummy.root.branch1.leaf1.subleaf1')
+	obj.get_subtree('dummy.root.branch3')
+	obj.is_root('dummy.root')
+	obj.in_tree('dummy.root')
+	obj.is_leaf('dummy.root')
+	obj.make_root('dummy.root.branch3')
+	obj.print_node('dummy.root.branch3')
+	obj.remove_prefix('dummy')
+	obj.rename_node('root.branch3.leaf1', 'root.branch3.mapleleaf2')
 
 	_EXH.build_ex_tree(no_print=no_print)
 	_EXH.print_ex_tree()
--- src/putil/tree.py
+++ src/putil/tree.py
@@ -30,27 +30,31 @@ setattr(mod_obj, '_EXH', putil.exh.ExHandle(putil.tree.Tree))
 exobj = getattr(mod_obj, '_EXH')
 obj = putil.tree.Tree()
 obj.add([
-	{'name':'root.branch1', 'data':list()},
-	{'name':'root.branch2', 'data':list()},
-	{'name':'root.branch1.leaf1', 'data':list()},
-	{'name':'root.branch1.leaf1.subleaf1', 'data':333},
-	{'name':'root.branch1.leaf2', 'data':'Hello world!'},
-	{'name':'root.branch1.leaf2.subleaf2', 'data':list()},
+	{'name':'dummy.root.branch1', 'data':list()},
+	{'name':'dummy.root.branch2', 'data':list()},
+	{'name':'dummy.root.branch1.leaf1', 'data':list()},
+	{'name':'dummy.root.branch1.leaf1.subleaf1', 'data':333},
+	{'name':'dummy.root.branch1.leaf2', 'data':'Hello world!'},
+	{'name':'dummy.root.branch1.leaf2.subleaf2', 'data':list()},
 ])
-obj.collapse('root.branch1')
-obj.copy_subtree('root.branch1', 'root.branch3')
-obj.delete('root.branch2')
-obj.flatten_subtree('root.branch1')
-obj.get_node('root')
-obj.get_node_children('root')
-obj.get_node_parent('root.branch1.leaf1.subleaf1')
-obj.get_children('root')
-obj.get_leafs('root')
-obj.get_data('root')
-obj.in_tree('root')
-obj.is_root('root')
-obj.is_leaf('root')
-obj.make_root('root.branch1.leaf2')
+obj.collapse('dummy.root.branch1')
+obj.copy_subtree('dummy.root.branch1', 'dummy.root.branch3')
+obj.delete('dummy.root.branch2')
+obj.flatten_subtree('dummy.root.branch1')
+obj.get_children('dummy.root')
+obj.get_data('dummy.root')
+obj.get_leafs('dummy.root')
+obj.get_node('dummy.root')
+obj.get_node_children('dummy.root')
+obj.get_node_parent('dummy.root.branch1.leaf1.subleaf1')
+obj.get_subtree('dummy.root.branch3')
+obj.is_root('dummy.root')
+obj.in_tree('dummy.root')
+obj.is_leaf('dummy.root')
+obj.make_root('dummy.root.branch3')
+obj.print_node('dummy.root.branch3')
+obj.remove_prefix('dummy')
+obj.rename_node('root.branch3.leaf1', 'root.branch3.mapleleaf2')
 exobj.build_ex_tree(no_print=True)
 exobj_tree = copy.deepcopy(exobj)
 ]]]
@@ -134,11 +138,11 @@ class Tree(object):	#pylint: disable=R0903
 	def _get_nodes(self):	#pylint: disable=C0111
 		return None if not self._db else sorted(self._db.keys())
 
-	def _get_root_node(self):	#pylint: disable=C0111
-		return None if not self.root_name else self.get_node(self.root_name)
-
 	def _get_root_name(self):	#pylint: disable=C0111
 		return self._root
+
+	def _get_root_node(self):	#pylint: disable=C0111
+		return None if not self.root_name else self.get_node(self.root_name)
 
 	def _node_in_tree(self, name):	#pylint: disable=C0111
 		self._exh.ex_add(name='node_not_in_tree', extype=RuntimeError, exmsg='Node *[node_name]* not in tree')
@@ -598,23 +602,6 @@ class Tree(object):	#pylint: disable=R0903
 		return [name]+[node for child in self.get_children(name) for node in self.get_subtree(child)]
 
 	@putil.check.check_argument(NodeName())
-	def in_tree(self, name):
-		"""
-		Return *True* if node name is in the tree, *False* otherwise
-
-		:param	name: Node name to search for
-		:type	name: string
-		:rtype	data: boolean
-
-		.. [[[cog cog.out(exobj_tree.get_sphinx_doc_for_member('in_tree')) ]]]
-
-		:raises: TypeError (Argument `name` is of the wrong type)
-
-		.. [[[end]]]
-		"""
-		return name in self._db
-
-	@putil.check.check_argument(NodeName())
 	def is_root(self, name):	#pylint: disable=C0111
 		"""
 		Root node flag, *True* if node is the root node (node with no ancestors), *False* otherwise
@@ -631,6 +618,23 @@ class Tree(object):	#pylint: disable=R0903
 		"""
 		self._node_in_tree(name)
 		return not self._db[name]['parent']
+
+	@putil.check.check_argument(NodeName())
+	def in_tree(self, name):
+		"""
+		Return *True* if node name is in the tree, *False* otherwise
+
+		:param	name: Node name to search for
+		:type	name: string
+		:rtype	data: boolean
+
+		.. [[[cog cog.out(exobj_tree.get_sphinx_doc_for_member('in_tree')) ]]]
+
+		:raises: TypeError (Argument `name` is of the wrong type)
+
+		.. [[[end]]]
+		"""
+		return name in self._db
 
 	@putil.check.check_argument(NodeName())
 	def is_leaf(self, name):	#pylint: disable=C0111
