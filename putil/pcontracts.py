43875504e22711529326bbdbdc262728caebf659
Pablo Acosta-Serafini <pmasdev@gmail.com>
2014-11-16 10:53:06 -0500

    Initial commit of putil.pcontracts module, with custom PyContracts contracts for API end-point data validation

---
+++ putil/pcontracts.py
@@ -0,0 +1,43 @@
+﻿# pcontracts.py
+# Copyright (c) 2013-2014 Pablo Acosta-Serafini
+# See LICENSE for details
+
+"""
+PyContracts custom contracts
+"""
+
+import os
+import contracts
+
+@contracts.new_contract
+def file_name(name):
+	""" Contract to validate a file name (i.e. file name does not have extraneous characters, etc.) """
+	msg = 'File name is not valid '
+	# Check that argument is a string
+	if not isinstance(name, str):
+		raise ValueError(msg)
+	# If file exists, argment is a valid file name, otherwise test if file can be created
+	# User may not have permission to write file, but call to os.access should not fail
+	# if the file name is corect
+	try:
+		if not os.path.exists(name):
+			os.access(name, os.W_OK)
+	except:
+		raise ValueError(msg)
+
+@contracts.new_contract
+def file_name_exists(name):
+	""" Contract to validate that a file name is valid (i.e. file name does not have extraneous characters, etc.) and that the file exists """
+	msg = 'File name is not valid '
+	# Check that argument is a string
+	if not isinstance(name, str):
+		raise ValueError(msg)
+	# Check that file name is valid
+	try:
+		os.path.exists(name)
+	except:
+		raise ValueError(msg)
+	# Check that file exists
+	if not os.path.exists(name):
+		raise ValueError('File does not exist')
+
---
+++ tests/test_pcontracts.py
@@ -0,0 +1,46 @@
+﻿# test_pcontracts.py	#pylint:disable=C0302
+# Copyright (c) 2014 Pablo Acosta-Serafini
+# See LICENSE for details
+
+"""
+putil.pcontracts unit tests
+"""
+
+import sys
+import pytest
+import contracts
+
+import putil.pcontracts	#pylint: disable=W0611
+
+def check_pcontract_exception(obj, args, extype, exmsg):
+	""" Triggers exception withing the Py.test environment and records value """
+	with pytest.raises(extype) as excinfo:
+		obj(**args)	#pylint: disable=W0142
+	if exmsg not in excinfo.value.error:
+		print excinfo.value.error
+	return exmsg in excinfo.value.error
+
+def test_file_name_contract():
+	""" Test for file_name custom contract """
+	@contracts.contract(sfn='file_name')
+	def func(sfn):
+		""" Sample function to test file_name custom contract """
+		return sfn
+	test_list = list()
+	test_list.append(check_pcontract_exception(func, {'sfn':3}, contracts.ContractNotRespected, 'File name is not valid'))
+	test_list.append(check_pcontract_exception(func, {'sfn':'test\0'}, contracts.ContractNotRespected, 'File name is not valid'))
+	func(sys.executable)	# Test with Python executable (should be portable across systems), file should be valid although not having permissions to write it
+	assert test_list == len(test_list)*[True]
+
+def test_file_name_exists_contract():
+	""" Test for file_name_exists custom contract """
+	@contracts.contract(sfn='file_name_exists')
+	def func(sfn):
+		""" Sample function to test file_name_exists custom contract """
+		return sfn
+	test_list = list()
+	test_list.append(check_pcontract_exception(func, {'sfn':3}, contracts.ContractNotRespected, 'File name is not valid'))
+	test_list.append(check_pcontract_exception(func, {'sfn':'test\0'}, contracts.ContractNotRespected, 'File name is not valid'))
+	test_list.append(check_pcontract_exception(func, {'sfn':'_file_does_not_exist'}, contracts.ContractNotRespected, 'File does not exist'))
+	func(sys.executable)	# Test with Python executable (should be portable across systems)
+	assert test_list == len(test_list)*[True]
