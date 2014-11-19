d55ea5a08016dbf5f86f531ca15d999a1ae88231
Pablo Acosta-Serafini <pmasdev@gmail.com>
2014-11-19 00:06:01 -0500

    pcontracts module: * Work in progress to have custom PyContracts contracts with exception   registering and custom error messages

--- putil/pcontracts.py
+++ putil/pcontracts.py
@@ -17,9 +17,158 @@ import decorator
 _CUSTOM_CONTRACTS = dict()
 
 ###
+# Functions
+###
+def new_contract(exdesc=None):	#pylint: disable=R0912
+	"""	New contract decorator constructor """
+	def wrapper(func):	#pylint: disable=R0912,R0914
+		""" Decorator """
+		# Make exdesc default if no argument passed
+		exdesc_int = exdesc if exdesc != None else {'argument_invalid':{'msg':'Argument `*[argument_name]*` is not valid'}}
+		# Pass to the custom contract, via a property, only the exception descriptions
+		func.exdesc = dict([(name, value['msg']) for name, value in exdesc_int.items()])
+		# Register custom contract
+		register_custom_contracts(func.__name__, exdesc_int)
+		# Apply PyContract decorator
+		return contracts.new_contract(func)
+	return wrapper
+
+
+def contract(**contract_args):	#pylint: disable=R0912
+	"""	Decorator constructor """
+	@decorator.decorator
+	def wrapper(func, *args, **kwargs):	#pylint: disable=R0912,R0914
+		""" Decorator """
+		param_dict = create_argument_dictionary(func, *args, **kwargs)
+		if not param_dict:
+			# An empty param_dict means there is a mimatch between passed parameters and defined function
+			# parameters, let Python interpreter take care of that case by returning function undecorated
+			return func
+		else:
+			# Register exceptions if exception handler object exists
+			exhobj = get_exh_obj()
+			if exhobj:
+				for param_name, param_contract in contract_args.items():
+					for exdict in get_contract_exception_message(param_contract).values():
+						exname = 'contract_{0}_{1}'.format(param_name, exdict['num'])
+						exhobj.add_exception(name=exname, extype=exdict['type'], exmsg=exdict['msg'].replace('*[argument_name]*', param_name))
+			# Actually validate arguments
+			try:
+				return contracts.contract_decorator(func, **contract_args)(*args, **kwargs)
+			except contracts.ContractNotRespected as eobj:
+				_, _, tbobj = sys.exc_info()
+				# Extract which function parameter triggered exception
+				param_name = re.search(r"'\w+'", eobj.error).group()[1:-1]	# re.search returns the string with quotes in it
+				# Raise exception
+				for exdict in get_contract_exception_message(contract_args[param_name]).values():
+					if exdict['msg'] in eobj.error:
+						exname = 'contract_{0}_{1}'.format(param_name, exdict['num'])
+						edata = {'field':exdict['field'], 'value':param_dict[param_name]} if exdict['field'] != 'argument_name' else None
+						break
+				else:
+					raise RuntimeError('Unable to translate contract exception message')
+				if exhobj:
+					exhobj.raise_exception_if(name=exname, condition=True, edata=edata)
+				else:
+					msg = exdict['msg'].replace('*[{0}]*'.format(exdict['field']), param_name if exdict['field'] == 'argument_name' else param_dict[param_name])	#pylint: disable=W0631
+					# Extract custom message from long PyContracts exception message (needed if exception handler is not used)
+					raise exdict['type'], exdict['type'](msg), tbobj
+			except:
+				# Re-raise exception if it was not due to invalid argument
+				raise
+	return wrapper
+
+
+def create_argument_dictionary(func, *args, **kwargs):
+	"""
+	Creates a dictionary where the keys are the argument names and the values are the passed arguments values (if any)
+	An empty dictionary is returned if an error is detected, such as more arguments than in the function definition, argument(s) defined by position and keyword, etc.
+	"""
+	# Capture parameters that have been explicitly specified in function call
+	try:
+		arg_dict = funcsigs.signature(func).bind_partial(*args, **kwargs).arguments
+	except:	#pylint: disable=W0702
+		return dict()
+	# Capture parameters that have not been explicitly specified but have default values
+	arguments = funcsigs.signature(func).parameters
+	for arg_name in arguments:
+		if (arguments[arg_name].default != funcsigs.Parameter.empty) and (arguments[arg_name].name not in arg_dict):
+			arg_dict[arguments[arg_name].name] = arguments[arg_name].default
+	return arg_dict
+
+
+def get_exh_obj():
+	""" Get exception handler object (if any) """
+	root_module = inspect.stack()[-1][0]
+	return root_module.f_locals.get('_EXH', None) if root_module else None
+
+
+def get_exdesc():
+	""" Get function attribute within function """
+	sobj = inspect.stack()
+	# First frame is own function (get_exdesc), next frame is the calling function, of which its name is needed
+	fname = sobj[1][3]
+	# Get globals variables, where function attributes reside
+	if fname in sobj[2][0].f_locals:
+		fobj = sobj[2][0].f_locals[fname]
+	elif fname in sobj[2][0].f_globals:
+		fobj = sobj[2][0].f_globals[fname]
+	else:
+		raise RuntimeError('Function object could not be found')
+	# Return function attribute created by new contract decorator
+	exdesc = getattr(fobj, 'exdesc')
+	return exdesc if len(exdesc) > 1 else exdesc[exdesc.keys()[0]]
+
+
+def get_replacement_token(msg):
+	""" Extract replacement token from exception message """
+	return None if not re.search(r'\*\[[\w|\W]+\]\*', msg) else re.search(r'\*\[[\w|\W]+\]\*', msg).group()[2:-2]
+
+
+def register_custom_contracts(contract_name, contract_exceptions):
+	""" Homogenize custom contract exception definition """
+	global _CUSTOM_CONTRACTS	#pylint: disable=W0602
+	# Validate arguments and homogenize contract exceptions
+	if not isinstance(contract_name, str):
+		raise TypeError('Argument `contract_name` is of the wrong type')
+	if (not isinstance(contract_exceptions, dict)) and (not isinstance(contract_exceptions, str)):
+		raise TypeError('Argument `contract_exceptions` is of the wrong type')
+	if isinstance(contract_exceptions, dict) and any([not isinstance(key, str) for key in contract_exceptions.keys()]):
+		raise TypeError('Contract exception name is of the wrong type')
+	for num, (exname, exvalue) in enumerate(contract_exceptions.items()):
+		if isinstance(exvalue, dict):
+			print set(exvalue.keys())
+		if isinstance(exvalue, dict) and (not ((set(exvalue.keys()) == set(['msg'])) or (set(exvalue.keys()) == set(['msg', 'type'])))):
+			raise TypeError('Contract exception is of the wrong type')
+		if isinstance(exvalue, dict) and ((not isinstance(exvalue.get('msg', ''), str)) or (not isinstance(exvalue.get('type', type), type))):
+			raise TypeError('Contract exception is of the wrong type')
+		if isinstance(exvalue, str):
+			contract_exceptions[exname] = {'num':num, 'msg':exvalue, 'type':RuntimeError, 'field':get_replacement_token(exvalue)}
+		else:
+			contract_exceptions[exname] = {'num':num, 'msg':exvalue['msg'], 'type':exvalue.get('type', RuntimeError), 'field':get_replacement_token(exvalue['msg'])}
+	# Verify that exception messages are unique
+	msgs = [exvalue['msg'] for exvalue in contract_exceptions.values()]
+	if len(set(msgs)) != len(msgs):
+		raise ValueError('Contract exception messages are not unique')
+	# Verify that a custom contract is not being redefined
+	if (contract_name in _CUSTOM_CONTRACTS) and (_CUSTOM_CONTRACTS[contract_name] != contract_exceptions):
+		raise RuntimeError('Attemp to redefine custrom contract `{0}`'.format(contract_name))
+	# Register new contract
+	_CUSTOM_CONTRACTS[contract_name] = contract_exceptions
+	return contract_exceptions
+
+
+def get_contract_exception_message(param_contracts):
+	""" Generate message for exception """
+	for contract_name, contract_exceptions in _CUSTOM_CONTRACTS.items():
+		if re.match(r'\<{0}\>'.format(contract_name), param_contracts):
+			return contract_exceptions
+	return {'num':0, 'msg':'Argument `*[argument_name]*` is not valid', 'type':RuntimeError, 'field':'argument_name'}
+
+
+###
 # Custom contracts definition
 ###
-
 @new_contract()
 def file_name(name):
 	""" Contract to validate a file name (i.e. file name does not have extraneous characters, etc.) """
@@ -37,7 +186,7 @@ def file_name(name):
 		raise ValueError(msg)
 
 
-@new_contract({\
+@new_contract({ \
 	'argument_invalid':{'msg':'Argument `*[argument_name]*` is not valid'},
 	'file_not_found': {'msg':'File `*[file_name]*` could not be found', 'type':IOError} \
 })
@@ -59,146 +208,4 @@ def file_name_exists(name):
 		raise ValueError(msg)
 
 
-###
-# Functions
-###
-def get_exdesc():
-	""" Get function attribute within function """
-	sobj = inspect.stack()
-	# First frame is own function (get_exdesc), next frame is the calling function, of which its name is needed
-	fname = sobj[1][3]
-	# Get globals variables, where function attributes reside
-	fobj = sobj[2][0].f_globals[fname]
-	# Return function attribute created by new contract decorator
-	exdesc = getattr(fobj, 'exdesc')
-	return exdesc if len(exdesc) > 1 else exdesc[exdesc.keys()[0]]
 
-
-def new_contract(exdesc=None):	#pylint: disable=R0912
-	"""	New contract decorator constructor """
-	def wrapper(func):	#pylint: disable=R0912,R0914
-		""" Decorator """
-		# Make exdesc default if no argument passed
-		exdesc_int = exdesc if exdesc != None else {'argument_invalid':{'msg':'Argument `*[argument_name]*` is not valid'}}
-		# Pass to the custom contract, via a property, only the exception descriptions
-		func.exdesc = dict([(name, value['msg']) for name, value in exdesc_int.items()])
-		# Register custom contract
-		register_custom_contracts(func.__name__, exdesc_int)
-		# Apply PyContract decorator
-		return contracts.new_contract(func)
-	return wrapper
-
-
-def register_custom_contracts(contract_name, contract_exceptions):
-	""" Homogenize custom contract exception definition """
-	global _CUSTOM_CONTRACTS	#pylint: disable=W0602
-	# Validate arguments and homogenize contract exceptions
-	if not isinstance(contract_name, str):
-		raise TypeError('Argument `contract_name` is of the wrong type')
-	if not isinstance(contract_exceptions, dict):
-		raise TypeError('Argument `contract_exceptions` is of the wrong type')
-	if any(not isinstance(key, str) for key in contract_exceptions.keys()):
-		raise TypeError('Contract exception name is of the wrong type')
-	for exname, exvalue in contract_exceptions.items():
-		if ((not isinstance(exvalue, str)) and (not isinstance(exvalue, dict))) or (isinstance(exvalue, dict) and ((set(exvalue.keys()) != set(['msg'])) or (set(exvalue.keys()) != set(['msg', 'type'])))) or \
-			(isinstance(exvalue, dict) and ((not isinstance(exvalue.get('msg', ''), str)) or (not isinstance(exvalue.get('type', type), str)))):
-			raise TypeError('Contract exception `{0}` is of the wrong type'.format(exname))
-		if isinstance(exvalue, str):
-			contract_exceptions[exname] = {'msg':exvalue, 'type':RuntimeError}
-		else:
-			contract_exceptions[exname] = {'msg':exvalue['msg'], 'type':exvalue.get('type', RuntimeError)}
-	# Verify that a custom contract is not being redefined
-	if (contract_name in _CUSTOM_CONTRACTS) and (_CUSTOM_CONTRACTS[contract_name] != contract_exceptions):
-		raise RuntimeError('Attemp to redefine custrom contract `{0}`'.format(contract_name))
-	# Register new contract
-	_CUSTOM_CONTRACTS[contract_name] = contract_exceptions
-	return contract_exceptions
-
-def get_exh_obj():
-	""" Get exception handler object (if any) """
-	root_module = inspect.stack()[-1][0]
-	return root_module.f_locals.get('_EXH', None) if root_module else None
-
-
-def get_replacement_token(msg):
-	""" Extract replacement token from exception message """
-	return None if not re.search(r'\*\[[\w|\W]+\]\*', msg).group() else re.search(r'\*\[[\w|\W]+\]\*', msg).group()[2:-2]
-
-
-def get_contract_exception_message(param_name, param_value, param_contracts):
-	""" Generate message for exception """
-	for custom_contract, contract_exceptions in _CUSTOM_CONTRACTS.items():
-		exmsgs = [exvalue['msg'] for exvalue in custom_exceptions.values()]
-		exfound = False
-		for exmsg in exmsgs:
-			if exmsg in param_contracts[param_name]:
-				contract_exceptions = contract_exceptions if isinstance(contract_exceptions, list) else [contract_exceptions]
-				ret = dict([(exmsg, (num, extype, get_replacement_token(exmsg), param_name if get_replacement_token(exmsg) == '*[argument_name]*' else param_value)) for num, (extype, exmsg) in enumerate(contract_exceptions)])
-				exfound = True
-				break
-		if exfound:
-			break
-	else:
-		ret = {'Argument `*[argument_name]*` is not valid':(0, RuntimeError, 'argument_name', param_name)}
-	return ret
-
-
-def create_argument_dictionary(func, *args, **kwargs):
-	"""
-	Creates a dictionary where the keys are the argument names and the values are the passed arguments values (if any)
-	An empty dictionary is returned if an error is detected, such as more arguments than in the function definition, argument(s) defined by position and keyword, etc.
-	"""
-	# Capture parameters that have been explicitly specified in function call
-	try:
-		arg_dict = funcsigs.signature(func).bind_partial(*args, **kwargs).arguments
-	except:	#pylint: disable=W0702
-		return dict()
-	# Capture parameters that have not been explicitly specified but have default values
-	arguments = funcsigs.signature(func).parameters
-	for arg_name in arguments:
-		if (arguments[arg_name].default != funcsigs.Parameter.empty) and (arguments[arg_name].name not in arg_dict):
-			arg_dict[arguments[arg_name].name] = arguments[arg_name].default
-	return arg_dict
-
-
-def contract(**contract_args):	#pylint: disable=R0912
-	"""	Decorator constructor """
-	@decorator.decorator
-	def wrapper(func, *args, **kwargs):	#pylint: disable=R0912,R0914
-		""" Decorator """
-		param_dict = create_argument_dictionary(func, *args, **kwargs)
-		if not param_dict:
-			# An empty param_dict means there is a mimatch between passed parameters and defined function
-			# parameters, let Python interpreter take care of that case by returning function undecorated
-			return func
-		else:
-			exhobj = get_exh_obj()
-			if exhobj:
-				for param_name in contract_args:
-					for exmsg, (exnum, extype, _, exvalue) in get_contract_exception_message(param_name, param_dict[param_name], contract_args).items():
-						exname = 'contract_{0}_{1}'.format(param_name, exnum)
-						exhobj.add_exception(name=exname, extype=extype, exmsg=exmsg.replace('*[argument_name]*', exvalue))
-			try:
-				return contracts.contract_decorator(func, **contract_args)(*args, **kwargs)
-			except contracts.ContractNotRespected as eobj:
-				_, _, tbobj = sys.exc_info()
-				# Extract which function parameter triggered exception
-				param_name = re.search(r"'\w+'", eobj.error).group()[1:-1]	# re.search returns the string with quotes in it
-				# Raise exception
-				for exmsg, (exnum, extype, exfield, exvalue) in get_contract_exception_message(param_name, param_dict[param_name], contract_args).items():
-					if exmsg in eobj.error:
-						exname = 'contract_{0}_{1}'.format(param_name, exnum)
-						edata = {'field':exfield, 'value':exvalue}
-						break
-				else:
-					raise RuntimeError('Unable to translate contract exception message')
-				if exhobj:
-					exhobj.raise_exception_if(name=exname, condition=True, edata=edata)
-				else:
-					msg = exmsg.replace('*[{0}]*'.format(exfield), exvalue)	#pylint: disable=W0631
-					# Extract custom message from long PyContracts exception message (needed if exception handler is not used)
-					raise extype, extype(msg), tbobj
-			except:
-				# Re-raise exception if it was not due to invalid argument
-				raise
-	return wrapper
--- tests/test_pcontracts.py
+++ tests/test_pcontracts.py
@@ -6,33 +6,80 @@
 putil.pcontracts unit tests
 """
 
-import sys
-import contracts
-
 import putil.test
 import putil.pcontracts	#pylint: disable=W0611
 
-def test_file_name_contract():
-	""" Test for file_name custom contract """
-	@contracts.contract(sfn='file_name')
-	def func(sfn):
-		""" Sample function to test file_name custom contract """
-		return sfn
+_EXH = 'Test global variable'
+def test_get_exh_obj():
+	""" Test get_txh_obj() function """
 	test_list = list()
-	test_list.append(putil.test.trigger_pcontract_exception(func, {'sfn':3}, 'File name is not valid'))
-	test_list.append(putil.test.trigger_pcontract_exception(func, {'sfn':'test\0'}, 'File name is not valid'))
-	func(sys.executable)	# Test with Python executable (should be portable across systems), file should be valid although not having permissions to write it
+	test_list.append(putil.pcontracts.get_exh_obj() == 'Test global variable')
+	del globals()['_EXH']
+	test_list.append(putil.pcontracts.get_exh_obj() == None)
+
+
+def sample_func_global():
+	""" Global test function to test get_exdesc() function """
+	tmp_global = putil.pcontracts.get_exdesc()
+	return tmp_global
+
+def test_get_exdesc():
+	""" Test get_exdesc() function """
+	def sample_func_local():
+		""" Local test function to test get_exdesc() function """
+		tmp_local = putil.pcontracts.get_exdesc()
+		return tmp_local
+	test_list = list()
+	sample_func_local.exdesc = 'Test local function property'
+	sample_func_global.exdesc = 'Test global function property'
+	test_list.append(sample_func_local() == 'Test local function property')
+	test_list.append(sample_func_global() == 'Test global function property')
+	del globals()['sample_func_global']
+	test_list.append(putil.test.trigger_exception(putil.pcontracts.get_exdesc, {}, RuntimeError, 'Function object could not be found'))
 	assert test_list == len(test_list)*[True]
 
-def test_file_name_exists_contract():
-	""" Test for file_name_exists custom contract """
-	@contracts.contract(sfn='file_name_exists')
-	def func(sfn):
-		""" Sample function to test file_name_exists custom contract """
-		return sfn
+
+def test_get_replacement_token():
+	""" Test get_replacement_token() function """
 	test_list = list()
-	test_list.append(putil.test.trigger_pcontract_exception(func, {'sfn':3}, 'File name is not valid'))
-	test_list.append(putil.test.trigger_pcontract_exception(func, {'sfn':'test\0'}, 'File name is not valid'))
-	test_list.append(putil.test.trigger_pcontract_exception(func, {'sfn':'_file_does_not_exist'}, 'File _file_does_not_exist could not be found'))
-	func(sys.executable)	# Test with Python executable (should be portable across systems)
+	test_list.append(putil.pcontracts.get_replacement_token('Argument `*[argument_name]*` could not be found') == 'argument_name')
+	test_list.append(putil.pcontracts.get_replacement_token('Argument `*file_name*` could not be found') == None)
 	assert test_list == len(test_list)*[True]
+
+
+def test_register_custom_contracts():
+	""" Test register_custom_contracts """
+	test_list = list()
+	test_list.append(putil.test.trigger_exception(putil.pcontracts.register_custom_contracts, {'contract_name':5, 'contract_exceptions':{}}, TypeError, 'Argument `contract_name` is of the wrong type'))
+	test_list.append(putil.test.trigger_exception(putil.pcontracts.register_custom_contracts, {'contract_name':'test', 'contract_exceptions':5}, TypeError, 'Argument `contract_exceptions` is of the wrong type'))
+	test_list.append(putil.test.trigger_exception(putil.pcontracts.register_custom_contracts, {'contract_name':'test', 'contract_exceptions':{5:'a', 6:'b'}}, TypeError, 'Contract exception name is of the wrong type'))
+	test_list.append(putil.test.trigger_exception(putil.pcontracts.register_custom_contracts, {'contract_name':'test', 'contract_exceptions':{'a':3.5}}, TypeError, 'Contract exception is of the wrong type'))
+	#test_list.append(putil.test.trigger_exception(putil.pcontracts.register_custom_contracts, {'contract_name':'test', 'contract_exceptions':{'a':'b'}}, TypeError, 'Contract exception is of the wrong type'))
+	test_list.append(putil.test.trigger_exception(putil.pcontracts.register_custom_contracts, {'contract_name':'test', 'contract_exceptions':{'msg':'b', 'x':RuntimeError}}, TypeError, 'Contract exception is of the wrong type'))
+	assert test_list == len(test_list)*[True]
+
+
+#def test_file_name_contract():
+#	""" Test for file_name custom contract """
+#	@contracts.contract(sfn='file_name')
+#	def func(sfn):
+#		""" Sample function to test file_name custom contract """
+#		return sfn
+#	test_list = list()
+#	test_list.append(putil.test.trigger_pcontract_exception(func, {'sfn':3}, 'File name is not valid'))
+#	test_list.append(putil.test.trigger_pcontract_exception(func, {'sfn':'test\0'}, 'File name is not valid'))
+#	func(sys.executable)	# Test with Python executable (should be portable across systems), file should be valid although not having permissions to write it
+#	assert test_list == len(test_list)*[True]
+#
+#def test_file_name_exists_contract():
+#	""" Test for file_name_exists custom contract """
+#	@contracts.contract(sfn='file_name_exists')
+#	def func(sfn):
+#		""" Sample function to test file_name_exists custom contract """
+#		return sfn
+#	test_list = list()
+#	test_list.append(putil.test.trigger_pcontract_exception(func, {'sfn':3}, 'File name is not valid'))
+#	test_list.append(putil.test.trigger_pcontract_exception(func, {'sfn':'test\0'}, 'File name is not valid'))
+#	test_list.append(putil.test.trigger_pcontract_exception(func, {'sfn':'_file_does_not_exist'}, 'File _file_does_not_exist could not be found'))
+#	func(sys.executable)	# Test with Python executable (should be portable across systems)
+#	assert test_list == len(test_list)*[True]
