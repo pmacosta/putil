f916aa72c564db342a1def0b50afa129103def21
Pablo Acosta-Serafini <pmasdev@gmail.com>
2014-12-19 10:52:22 -0500

    Added test support directory with files/modules used for unit testing

---
+++ tests/support/my_module1.py
@@ -0,0 +1,24 @@
+﻿""" my_module1 module """
+
+def enclosing_func(offset):
+	""" Test function to see if code detects enclosures """
+	def closure_func(value):
+		"""
+		Actual closure function, should be reported as:
+		putil.my_module.enclosing_func.closure_func
+		"""
+		return offset+value
+	return closure_func
+
+class MyClass(object):	#pylint: disable=R0903
+	""" Test class """
+	def __init__(self):
+		self._value = None
+	def _getter_func(self):
+		""" Simple getter method """
+		return self._value
+	def _setter_func(self, value):
+		""" Simple setter method """
+		self._value = value
+	value = property(_getter_func, _setter_func, None, doc='Value property')
+
---
+++ tests/support/my_module2.py
@@ -0,0 +1,25 @@
+﻿""" my_module2 module """
+def yet_another_enclosing_func(offset, setter_func):
+	""" Test function to see if code detects enclosures """
+	def yet_another_closure_func(self):
+		"""
+		Actual closure function, should be reported as:
+		putil.my_module2.yet_another_enclosing_func.yet_another_closure_func
+		"""
+		return offset+self._value	#pylint: disable=W0212
+	return property(yet_another_closure_func, setter_func, None, doc='Value property')
+
+
+class MyClassThatErrorsWhenTraced(object):	#pylint: disable=R0903
+	""" Test class that so far raises exception when 'traced' """
+	def __init__(self):
+		self._value = 10
+	def _setter_func(self, value):
+		""" Simple setter method """
+		self._value = value
+	def _setter_func2(self, value):
+		""" Simple setter method """
+		self._value = value
+	value = yet_another_enclosing_func(5, _setter_func)
+	line = property(lambda self: self._value+10, _setter_func2)
+
