ba8bacb4897e0f808155cd66c9a595fb5133ab47
Pablo Acosta-Serafini <pmasdev@gmail.com>
2015-01-23 20:30:00 -0500

    exdoc module: improved a unit test

---
+++ tests/support/exdoc_support_module_3.py
@@ -0,0 +1,32 @@
+﻿# pinspect_support_module_1.py
+# Copyright (c) 2013-2015 Pablo Acosta-Serafini
+# See LICENSE for details
+# pylint: disable=C0111,W0212
+
+import putil.exh
+
+def simple_property_generator():
+	""" Function to test if properties done via enclosed functions are properly detected """
+	def fget(self):
+		""" Actual getter function """
+		self._exobj.add_exception(exname='illegal_value', extype=TypeError, exmsg='Cannot get value')
+		return self._value
+	return property(fget)
+
+
+class Class1(object):	#pylint: disable=R0903
+	""" First (of 2) class that has a property action function defined by a factory """
+	def __init__(self):
+		self._exobj = putil.exh.get_exh_obj() if putil.exh.get_exh_obj() else putil.exh.ExHandle()
+		self._value = None
+
+	value1 = simple_property_generator()
+
+
+class Class2(object):	#pylint: disable=R0903
+	""" Second (of 2) class that has a property action function defined by a factory """
+	def __init__(self):
+		self._exobj = putil.exh.get_exh_obj() if putil.exh.get_exh_obj() else putil.exh.ExHandle()
+		self._value = None
+
+	value2 = simple_property_generator()
--- tests/test_exdoc.py
+++ tests/test_exdoc.py
@@ -13,6 +13,7 @@ import putil.exh
 import putil.test
 import putil.exdoc
 import exdoc_support_module_1
+import exdoc_support_module_3
 
 ###
 # Functions
@@ -30,6 +31,14 @@ def load_support_module():
 			raise
 	else:
 		__import__(module_name)
+
+def trace_error_class():
+	""" Trace classes that use the same getter function """
+	if putil.exh.get_exh_obj():
+		putil.exh.del_exh_obj()
+	putil.exh.set_exh_obj(putil.exh.ExHandle())
+	obj = exdoc_support_module_3.Class1()
+	obj.value1	#pylint: disable=W0104
 
 def trace_class():
 	""" Trace support module class """
@@ -63,26 +72,23 @@ def trace_module_functions():
 
 def test_exdoc_errors():
 	""" Test exdoc data validation """
-	test_list = list()
 	exobj = putil.exh.ExHandle()
 	def func1():	#pylint: disable=C0111,W0612
 		exobj.add_exception('first_exception', TypeError, 'This is the first exception')
 	func1()
 	obj = putil.exdoc.ExDoc
-	test_list.append(putil.test.trigger_exception(obj, {'exh_obj':5, 'no_print':False, 'trace_name':'hello'}, TypeError, 'Argument `exh_obj` is not valid'))
-	test_list.append(putil.test.trigger_exception(obj, {'exh_obj':putil.exh.ExHandle(), 'no_print':False, 'trace_name':'hello'}, ValueError, 'Object of argument `exh_obj` does not have any exception trace information'))
-	test_list.append(putil.test.trigger_exception(obj, {'exh_obj':exobj, 'no_print':5, 'trace_name':'hello'}, TypeError, 'Argument `no_print` is not valid'))
-	test_list.append(putil.test.trigger_exception(obj, {'exh_obj':exobj, 'no_print':False, 'trace_name':5}, TypeError, 'Argument `trace_name` is not valid'))
+	putil.test.assert_exception(obj, {'exh_obj':5, 'no_print':False, 'trace_name':'hello'}, TypeError, 'Argument `exh_obj` is not valid')
+	putil.test.assert_exception(obj, {'exh_obj':putil.exh.ExHandle(), 'no_print':False, 'trace_name':'hello'}, ValueError, 'Object of argument `exh_obj` does not have any exception trace information')
+	putil.test.assert_exception(obj, {'exh_obj':exobj, 'no_print':5, 'trace_name':'hello'}, TypeError, 'Argument `no_print` is not valid')
+	putil.test.assert_exception(obj, {'exh_obj':exobj, 'no_print':False, 'trace_name':5}, TypeError, 'Argument `trace_name` is not valid')
 	putil.exdoc.ExDoc(exobj, True, 'hello', -1)
-	assert test_list == [True]*len(test_list)
 
 def test_build_ex_tree():
 	""" Test _build_ex_tree() function """
-	test_list = list()
 	trace_class()
 	exdocobj = putil.exdoc.ExDoc(exh_obj=putil.exh.get_exh_obj(), no_print=True, trace_name='exdoc_support_module_1.ExceptionAutoDocClass', _step=0)
 	putil.exh.del_exh_obj()
-	test_list.append(str(exdocobj._tobj) == \
+	assert str(exdocobj._tobj) == \
 		u'exdoc_support_module_1.ExceptionAutoDocClass\n'.encode('utf-8') + \
 		u'├__init__ (*)\n'.encode('utf-8') + \
 		u'├divide (*)\n'.encode('utf-8') + \
@@ -93,15 +99,17 @@ def test_build_ex_tree():
 		u'├value2[fset] (*)\n'.encode('utf-8') + \
 		u'├value3[fdel] (*)\n'.encode('utf-8') + \
 		u'├value3[fget] (*)\n'.encode('utf-8') + \
-		u'└value3[fset] (*)'.encode('utf-8'))
+		u'└value3[fset] (*)'.encode('utf-8')
 	trace_module_functions()
 	exdocobj = putil.exdoc.ExDoc(exh_obj=putil.exh.get_exh_obj(), no_print=True, trace_name='exdoc_support_module_1', _step=0)
 	putil.exh.del_exh_obj()
-	test_list.append(str(exdocobj._tobj) == \
+	assert str(exdocobj._tobj) == \
 		u'exdoc_support_module_1\n'.encode('utf-8') + \
 		u'├probe (*)\n'.encode('utf-8') + \
 		u'├read (*)\n'.encode('utf-8') + \
 		u'└write (*)\n'.encode('utf-8') + \
 		u' └exdoc_support_module_1._write\n'.encode('utf-8') + \
-		u'  └exdoc_support_module_1._validate_arguments (*)'.encode('utf-8'))
-	assert test_list == [True]*len(test_list)
+		u'  └exdoc_support_module_1._validate_arguments (*)'.encode('utf-8')
+	trace_error_class()
+	exobj = putil.exh.get_exh_obj()
+	putil.test.assert_exception(putil.exdoc.ExDoc, {'exh_obj':exobj, 'trace_name':'exdoc_support_module_3', 'no_print':True, '_step':0}, RuntimeError, 'Functions performing actions for multiple properties not supported')
