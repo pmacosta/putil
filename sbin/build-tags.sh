8978a5e172504d066da2877f9e8b86095aeb9517
Pablo Acosta-Serafini <pmasdev@gmail.com>
2015-02-13 16:42:33 -0500

    Fixed bug in build-tags.sh file that would not correctly detect package name

--- sbin/build-tags.sh
+++ sbin/build-tags.sh
@@ -8,6 +8,6 @@ while [ -h "$source" ]; do # resolve $source until the file is no longer a symli
 done
 dir="$( cd -P "$( dirname "$source" )" && pwd )"
 package_root=$(dirname ${dir})
-package_name=$(basename ${dir})
+package_name=$(basename ${package_root})
 ctags -V --tag-relative -f $package_root/tags -R $package_root/${package_name}/*.py $package_root/tests/*.py
 # */2 * * * * /proj/ad9625_e_expctl/sos_pacosta/pacosta/sim/bin/build_tags.sh &>/home/pacosta/log/cheetah_tags.log
