6bb1f6ebff294d96824abad63650781259dc8e43
Pablo Acosta-Serafini <pmasdev@gmail.com>
2015-02-13 15:23:52 -0500

    Updated hooks symlink convenience script so that it can be called from any directory

--- .hooks/make-links.sh
+++ .hooks/make-links.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-hook_dir=../.git/hooks
-hooks=(pre-commit)
+# Find directory where script is (from http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in)
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+	DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+	SOURCE="$(readlink "$SOURCE")"
+	[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+repo_dir=$(dirname ${DIR})
+
 cpwd=${PWD}
-cd ${hook_dir}
+git_hooks_dir=${repo_dir}/.git/hooks
+hooks=(pre-commit)
+cd ${git_hooks_dir}
 for hook in ${hooks[@]}; do
-	ln -s -f ${cpwd}/${hook} ${hook}
+	ln -s -f ${DIR}/${hook} ${hook}
 done
 cd ${cpwd}
